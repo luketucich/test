8ecfebb054d6c54de4dbc8f7fff23d66949fde5a
luketucich <ltucich@gmail.com>
2024-10-06 21:57:50 -0400

    Created a Webpack template

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+// webpack.prod.js
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+});
